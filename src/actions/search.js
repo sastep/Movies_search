3e8b4a7a81c5d271eeed4fdfbca1b03c0b825f13
Smbat <smbat.93@mail.ru>
2017-06-27 18:19:04 +0400

    Project|All(fix): End of project

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const searchMovie = title => dispatch => axios.get(`http://www.omdbapi.com/?apikey=fcbd49b5&s=${title}`)
-  .then(result => dispatch({ type: 'SEARCH_MOVIE_SUCCESS', payload: result }))
+  .then(result => dispatch({ type: 'SEARCH_MOVIE_SUCCESS', payload: result, title }))
   .catch(error => dispatch({ type: 'SEARCH_MOVIE_FAIL', error }));
 
 export default searchMovie;
--- src/reducers/search.js
+++ src/reducers/search.js
@@ -1,7 +1,7 @@
 const Search = (state = {}, action) => {
   switch (action.type) {
     case 'SEARCH_MOVIE_SUCCESS':
-      return { ...state, movies: action.payload };
+      return { ...state, movies: action.payload, searchTitle: action.title };
     case 'SEARCH_MOVIE_FAIL':
       return { ...state, error: action.error };
     default:
